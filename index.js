9520ddb953b6ebf4e4bda460254f6d29a18a75b6
Nifer Kilakila <nifercritter@gmail.com>
2022-06-29 20:53:54 -0700

    started logic mapping, no code yet

--- index.js
+++ index.js
@@ -1,4 +1,52 @@
 // Packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateMarkdown = require('./utils/generateMarkdown');
+const generatePage = require('./utils/generatePage');
+
+
+// Parent Constructor class 
+const PersonQuestions = [
+    {
+        type: 'input',
+        message: 'Please enter name of team member',
+        name: 'fName',
+        default: 'Loulou'
+    },
+    {
+        type: 'input',
+        message: 'What is their ID number?',
+        name: 'idNum',
+        default: '8675309'
+    }, 
+    {
+        type: 'input',
+        message: 'What is their email address?',
+        name: 'email',
+        default: 'totallycool@yeah.com'
+    }
+];
+
+
+//  Sub-class Questions
+// Team Manager -- office number
+{
+    type: 'input',
+    message: 'What is their office number?',
+    name: 'officeNum',
+    default: '42'
+}
+// Engineer -- gitHub username
+{
+    type: 'input',
+    message: 'What is their Github user name?',
+    name: 'githubUser',
+    default: 'koolkid'
+}
+// Intern -- school
+{
+    type: 'input',
+    message: 'What is the name of their school?',
+    name: 'school',
+    default: 'UCSF Berkeley'
+}
+
